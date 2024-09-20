302d2068f92a624afa9b1cc61a8e976c261e287e
senpaiblue <singhsakcham29@email.com>
2024-09-21 01:47:21 +0530

    footer_fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex lg:flex-row flex-col text-center justify-center lg:justify-between w-full items-center px-0 lg:gap-y-0 gap-y-4 lg:px-32 py-12 bg-black/[0.96]">
       <p className="font-normal text-white">
-        <TextGenerateEffect words=" © 2024 Tradepulse ventures pvt Ltd. All rights reserved." />
+        <TextGenerateEffect words=" © 2024 VideoEditor ANMOL" />
       </p>
       <div className="flex items-center gap-x-6 justify-center">
         <a href="#" target="_blank" className="font-normal text-white">
